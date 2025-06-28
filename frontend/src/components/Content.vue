ea8e9f11bd9837d4c6f5abb3d3f8896f6144a20c
Rolan <markarovr@gmail.com>
2025-06-29 01:29:19 +0400

    clear app

---
+++ frontend/src/components/Content.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+ <div>
+  aloha
+ </div>
+
+
+ 
+</template>
+
+<style scoped>
+
+</style>
