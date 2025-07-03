e16c0cd0e523b6ff0002a15b5c29d6b1821d1669
Rolan <markarovr@gmail.com>
2025-07-03 23:58:13 +0400

    server configuration

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,21 +5,22 @@ const { sendAdminReport } = require('../utils/mailer');
 
 
 router.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
-
+  console.log( req.body)
+    const { name, email, password, phone } = req.body;
+ 
   try {
     const exists = await User.findOne({ email });
-    if (exists) return res.status(400).json({ message: 'Email уже зарегистрирован' });
+    if (exists) return res.status(400).json({ message: 'Email already exist' });
 
-    const user = new User({ name, email, password });
+    const user = new User({ name, email, password, phone });
     await user.save();
 
     await sendAdminReport(user);
 
-    res.status(201).json({ message: 'Пользователь зарегистрирован' });
+    res.status(200).json({ message: 'User Registred' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Ошибка сервера' });
+    res.status(500).json({ message: 'Server Error' });
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors());
+const multer = require('multer');
+
+const upload = multer();
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB подключена'));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+app.use(express.json());
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => console.log('MongoDB connected'));
 
 app.use('/api', require('./routes/auth'));
 
